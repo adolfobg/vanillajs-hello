e57a6f8bb8728d7f5c8565ae769dcf8c3c98dc32
ADOLFO BALSEIRO <abalseiro@hotmail.com>
2022-10-19 20:00:54 +0000

    Trabajo finalizado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,21 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#excuse").innerHTML = phrase("#excuse");
+};
+let phrase = () => {
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let whoindx = Math.floor(Math.random() * who.length);
+  let whatindx = Math.floor(Math.random() * what.length);
+  let whenindx = Math.floor(Math.random() * when.length);
+  return who[whoindx] + " " + what[whatindx] + " " + when[whenindx] + ".";
 };
